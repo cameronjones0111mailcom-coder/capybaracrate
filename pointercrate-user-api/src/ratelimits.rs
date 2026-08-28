b928397cbeb1910fce2c0fbf02222b05a19583ea
cameronjones0111mailcom-coder <cameronjones0111mail.com@gmail.com>
2026-08-28 18:43:38 -0400

    Update ratelimits.rs

--- pointercrate-user-api/src/ratelimits.rs
+++ pointercrate-user-api/src/ratelimits.rs
@@ -4,8 +4,8 @@ use std::net::IpAddr;
 
 ratelimits! {
     UserRatelimits {
-        registrations[1u32 per 86400 per IpAddr] => tr("error-user-ratelimit-registration"),
-        soft_registrations[5u32 per 21600 per IpAddr] => tr("error-user-ratelimit-soft-registration"),
-        login_attempts[3u32 per 1800 per IpAddr] => tr("error-user-ratelimit-login"),
+        registrations[100u32 per 60 per IpAddr] => tr("error-user-ratelimit-registration"),
+        soft_registrations[100u32 per 60 per IpAddr] => tr("error-user-ratelimit-soft-registration"),
+        login_attempts[100u32 per 60 per IpAddr] => tr("error-user-ratelimit-login"),
     }
 }
